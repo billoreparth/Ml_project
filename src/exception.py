883896849f5c000afd61f1aeedfc457e39bec628
Parth Billore <billoreparth80@gmail.com>
2025-02-28 17:46:39 +0530

    logger and exception commit

---
+++ src/exception.py
@@ -0,0 +1,28 @@
+import sys 
+import logging 
+
+
+
+def error_message(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    exc_message="Error occured in python script name [{0}] and line number is [{1}] , error message is [{2}] ".format(
+        file_name,exc_tb.tb_lineno,str(error))
+    
+    return exc_message
+    
+class CustomException(Exception):
+    def __init__(self,exc_message,error_detail:sys):
+        # super().__init__(exc_message)
+        self.exc_message=error_message(exc_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.exc_message
+
+
+if __name__=="__main__":
+    try:
+        x=1/0
+    except Exception as e :
+        logging.info('zero division error')
+        raise CustomException(e,sys)
---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging 
+import os 
+from datetime import datetime 
+
+log_file = f"{datetime.now().strftime('%d_%m_%Y__%H_%M_%S')}.log"
+log_path=os.path.join(os.getcwd(),"logs",log_file)
+os.makedirs(log_path,exist_ok=True)
+
+log_file_path=os.path.join(log_path,log_file)
+
+logging.basicConfig(
+    filename=log_file_path,
+    format='[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+
+if __name__=='__main__':
+    logging.info(
+     'this is testing login'
+    )
